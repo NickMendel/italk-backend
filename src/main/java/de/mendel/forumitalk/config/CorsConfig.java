255f2144b92497583079235b4ac62af22d1c86c2
NickMendel <nick.mendel@student.htw-berlin.de>
2023-10-31 14:16:58 +0100

    Removed OPTIONS

--- src/main/java/de/mendel/forumitalk/config/CorsConfig.java
+++ src/main/java/de/mendel/forumitalk/config/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000")
-                .allowedMethods("GET", "PUT", "POST", "DELETE", "OPTIONS")
+                .allowedMethods("GET", "PUT", "POST", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
