b4f95a20f2ad02cc3dafbe59562986f01735fe46
NickMendel <nick.mendel@student.htw-berlin.de>
2023-11-02 16:08:41 +0100

    implementation register, email and password update

--- src/main/java/de/mendel/forumitalk/controller/UserController.java
+++ src/main/java/de/mendel/forumitalk/controller/UserController.java
@@ -1,8 +1,9 @@
 package de.mendel.forumitalk.controller;
 
-import de.mendel.forumitalk.dto.UserDto;
-import de.mendel.forumitalk.exceptions.EmailInUseException;
-import de.mendel.forumitalk.exceptions.UsernameInUseException;
+import de.mendel.forumitalk.dto.ChangePasswordRequest;
+import de.mendel.forumitalk.dto.UserRegistrationRequest;
+import de.mendel.forumitalk.dto.UserUpdateEmailRequest;
+import de.mendel.forumitalk.model.User;
 import de.mendel.forumitalk.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v1/users/")
+@RequestMapping("api/v1/users")
 public class UserController {
 
     private final UserService service;
@@ -22,37 +23,30 @@ public class UserController {
         this.service = service;
     }
 
-    @PostMapping
-    public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto) {
-        try {
-            if (service.isUsernameTaken(userDto.getUsername())) {
-                throw new UsernameInUseException("Username is already taken");
-            }
+    @PostMapping("/register")
+    public ResponseEntity<String> createUser(@RequestBody UserRegistrationRequest userRegistrationRequest) {
+        service.registerNewUser(userRegistrationRequest);
+        return ResponseEntity.status(HttpStatus.OK).body("User created successfully!");
+    }
 
-            if (service.isEmailTaken(userDto.getEmail())) {
-                throw new EmailInUseException("Email is already in use");
-            }
-            UserDto createdUser = service.createUser(userDto);
-            return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
-        } catch (UsernameInUseException | EmailInUseException e) {
-            UserDto errorDto = new UserDto();
-            errorDto.setError(e.getMessage());
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorDto);
-        }
+    @PostMapping("/{id}/profile/change-password")
+    public ResponseEntity<String> changePassword(@PathVariable Long id, @RequestBody ChangePasswordRequest changePasswordRequest) {
+        service.changePassword(id, changePasswordRequest);
+        return ResponseEntity.ok("Password changed successfully!");
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<UserDto> getUserById(@PathVariable Long id) {
-        UserDto userDto = service.getUsersById(id);
-        if (userDto != null) {
-            return ResponseEntity.ok(userDto);
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        User user = service.getUserById(id);
+        if (user != null) {
+            return ResponseEntity.ok(user);
         }
         return ResponseEntity.notFound().build();
     }
 
     @GetMapping
-    public ResponseEntity<List<UserDto>> getAllUsers() {
-        List<UserDto> users = service.getAllUsers();
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = service.getAllUsers();
         return ResponseEntity.ok(users);
     }
 
@@ -62,12 +56,10 @@ public class UserController {
         return ResponseEntity.noContent().build();
     }
 
-    @PutMapping
-    public ResponseEntity<UserDto> updateUser(@RequestBody UserDto userDto) {
-        UserDto updatedUser = service.updateUser(userDto);
-        if (updatedUser != null) {
-            return ResponseEntity.ok(updatedUser);
-        }
-        return ResponseEntity.notFound().build();
+    @PutMapping("/{id}/profile/update-email")
+    public ResponseEntity<String> updateUser(@PathVariable Long id, @RequestBody UserUpdateEmailRequest userUpdateEmailRequest) {
+        User currentUser = service.getUserById(id);
+        service.updateUserEmail(currentUser, userUpdateEmailRequest);
+        return ResponseEntity.ok("User updated successfully!");
     }
 }
